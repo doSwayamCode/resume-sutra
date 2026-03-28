873290afc63d1a8b40f27be5daa5acac9ef4d06e
Swayam Gupta <swayamgupta999@gmail.com>
2026-03-28 17:38:22 +0530

    vercel ana

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Builder from "./pages/Builder";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/builder" element={<Builder />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/builder" element={<Builder />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
